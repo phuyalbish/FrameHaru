630bd2a01da244910aba1faab1f3607fc179cad3
phuyalbish <phuyalbish@gmail.com>
2026-02-21 16:33:34 +0545

    Update

--- .wrangler/tmp/bundle-kTl0jQ/middleware-insertion-facade.js
+++
@@ -1,4 +0,0 @@
-				import { __facade_registerInternal__ } from "/Users/bibekphuyal/.nvm/versions/node/v23.3.0/lib/node_modules/wrangler/templates/middleware/loader-sw.ts";
-				import * as __MIDDLEWARE_0__ from "/Users/bibekphuyal/.nvm/versions/node/v23.3.0/lib/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
-import * as __MIDDLEWARE_1__ from "/Users/bibekphuyal/.nvm/versions/node/v23.3.0/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
-				__facade_registerInternal__([__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default])
